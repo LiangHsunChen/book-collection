1a64c3c43af322129064370827b4db139c0acc24
LiangHsunChen <lianghsunchen@gmail.com>
2024-06-26 23:15:05 +1000

    Created view_book view to include ViewBookComponent

---
+++ application/views/view_book.php
@@ -0,0 +1,24 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coding Challenge</title>
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/app.css'); ?>">
+</head>
+<body>
+    <div id="view-book-component"></div>
+
+    <script>
+        window.book = <?php echo json_encode($book); ?>;
+    </script>
+
+    <script src="<?php echo base_url('assets/js/chunk-vendors.js'); ?>"></script>
+    <script src="<?php echo base_url('assets/js/app.js'); ?>"></script>
+</body>
+</html>
+
